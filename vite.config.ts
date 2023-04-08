60c24ce05f658fe49c9c587499b932c7810408f6
K.Himeno <6715229+Himenon@users.noreply.github.com>
2023-04-08 12:16:00 +0900

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA({
+    injectRegister: "auto",
+    minify: false,
+    registerType: "autoUpdate",
+    /**
+     * @see https://vite-pwa-org.netlify.app/guide/inject-manifest.html
+     */
+    srcDir: "src",
+    filename: "sw.ts",
+    devOptions: {
+      enabled: true,
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    }
+  })],
+});
